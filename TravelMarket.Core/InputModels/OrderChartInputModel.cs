4d90d0641eafe14df4df40e3fd7bb1ab7ca622d7
Путинцев Дмитрий <putdv@mail.ru>
2025-11-10 23:22:46 +0300

    добавлено модальное окно корзины. добавлена новая OrderChartInputModel. добавлен новый сервис OrderService

---
+++ TravelMarket.Core/InputModels/OrderChartInputModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelMarket.Core.Dtos;
+
+namespace TravelMarket.Core.InputModels
+{
+    public class OrderChartInputModel
+    {
+        [Required(ErrorMessage ="Заполнение паспортных данных для заказа путевки обязательно")]
+        public string Passport { get; set; }
+
+        public List<OrderDto> Orders { get; set; } = new List<OrderDto>();
+    }
+}
